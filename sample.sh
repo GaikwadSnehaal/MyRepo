cbc0b6c50db185361b8dd5346e819f978c6581de
snehal <snehal.gaikwad8100@gmail.com>
2023-11-27 15:47:02 +0530

    Commit

--- sample.sh
+++ sample.sh
@@ -2,4 +2,5 @@ echo "Hi"
 echo "Date:" $(date +%d/%m/%y)
 echo "PWD is:" $(pwd)
 echo "Home directory for" $(whoami) $(echo "is") $(echo $HOME)
-echo "Default shell: $(SHELL)
+echo "Default shell: $(SHELL)"
+echo "Hi snail, How are you?"
